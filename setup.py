88b873689a0d44f5c4309c77a40fa49edb1d2df0
Rodrigo Mosconi <mosconi.rmg@gmail.com>
2016-11-24 00:35:48 -0200

    conversão para python

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='pucmestrado',
+    version='0.1',
+    author='Rodrigo Mosconi',
+    author_email='rmosconi@inf.puc-rio.br',
+    packages=['roadef'],
+    package_dir={'roadef':'src/roadef'},
+)
