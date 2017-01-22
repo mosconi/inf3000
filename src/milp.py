09be9841e1956002e56517132305d721cd0d7b80
Rodrigo Mosconi <mosconi.rmg@gmail.com>
2017-01-22 12:30:08 -0200

    ajuste nas constraints

--- src/milp.py
+++ src/milp.py
@@ -7,15 +7,18 @@ from time import time
 from gurobipy import *
 
 def usage():
-    print("%s [-n name] -m modelfile -a assignfile\n" % sys.argv[0])
+    print("%s [-n name] [-o outputfile ] [-s] [-q] -m modelfile -a assignfile\n" % sys.argv[0])
 
 modelfile=None
 assignfile=None
 name="roadef"
 outputfile=None
+savemodel=False
+verbose=True
+tex=True
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:],"n:m:a:o:h",["name=","model=","assign=","output=","help"])
+    opts, args = getopt.getopt(sys.argv[1:],"tqsn:m:a:o:h",["tex","quiet","save","name=","model=","assign=","output=","help"])
 except getopt.GetoptError as err:
     print(err)
     usage()
@@ -33,6 +36,12 @@ for o,a in opts:
         name = a
     elif o in ( "-o", "--output"):
         outputfile = a
+    elif o in ( "-s", "--save"):
+        savemodel=True
+    elif o in ( "-q", "--quiet"):
+        verbose=False
+    elif o in ( "-t", "--tex"):
+        tex=True
     else:
         assert False, "unhandled option"
     
@@ -47,6 +56,8 @@ if not os.path.exists(modelfile):
 
 if not os.path.exists(assignfile):
     sys.exit("File %s not found" % assignfile)
+
+all_begin = time()
 
 T=[]
 Wlc=[]
@@ -82,9 +93,9 @@ for m in range(nmach):
     L[l.pop(0)].append(m) # location
     C.append(l[:nres])
     del(l[:nres])
-    SC.append(l[:nres])
+    C_bar.append(l[:nres])
     del(l[:nres])
-    M.append(l)
+    MU.append(l)
 
 # remove empty neighborhood/locations
 L = [l for l in L if l]
@@ -92,8 +103,8 @@ N = [n for n in N if n]
 
     
 C=np.array(C)
-C_bar=np.array(SC)
-mu=np.array(M)
+C_bar=np.array(C_bar)
+MU=np.array(MU)
 
 nserv = lines.pop(0)[0]
 delta=[0 for s in range(nserv)]
@@ -114,7 +125,7 @@ for p in range(nproc):
     S[l.pop(0)].append(p)
     R.append(l[:nres])
     del(l[:nres])
-    rho.append(l[0])
+    RHO.append(l[0])
 
 nbal = lines.pop(0)[0]
 bT=np.zeros((nres,nres),dtype=np.int32)
@@ -144,198 +155,212 @@ for p in range(nproc):
 
 T=np.array(T)
 
-print(">> problem size")
-print(">>> resources ",nres)
-print(">>> transisent ",T.sum())
-print(">>> machines ",nmach)
-print(">>> process ",nproc)
-print(">>> services ",nserv)
-print(">>> balances ",nbal)
-print(">>> neighborhood ",len(N))
-print(">>> locations ",len(L))
+if verbose: print(">> problem size")
+if verbose: print(">>> resources ",nres)
+if verbose: print(">>> transisent ",T.sum())
+if verbose: print(">>> machines ",nmach)
+if verbose: print(">>> process ",nproc)
+if verbose: print(">>> services ",nserv)
+if verbose: print(">>> balances ",nbal)
+if verbose: print(">>> neighborhood ",len(N))
+if verbose: print(">>> locations ",len(L))
+
+mdl_start = time()
+load_time = mdl_start - all_begin
 
 mdl=Model(name)
+if not verbose: mdl.Params.OutputFlag=0
 
 mdl.ModelSense = GRB.MINIMIZE
 
-print("creating x vars",flush=True)
+if verbose: print("creating x vars",flush=True)
 x = mdl.addVars(nproc,nmach,vtype=GRB.BINARY,lb=0,ub=1,name="x")
 
-print("starting x[p,m] as x_bar[p,m]",flush=True)
+if verbose: print("starting x[p,m] as x_bar[p,m]",flush=True)
 for p in range(nproc):
     for m in range(nmach):
         x[p,m].start = x_bar[p,m]
 
-print("creating z- vars",flush=True)
-z_minus = mdl.addVars(nproc,nmach,name="z-",vtype=GRB.INTEGER,lb=0,ub=1)
+if verbose: print("creating z- vars",flush=True)
+z_minus = mdl.addVars(nproc,nmach,name="z-",vtype=GRB.BINARY)
 
-print("creating z+ vars",flush=True)
-z_plus = mdl.addVars(nproc,nmach,name="z+",vtype=GRB.INTEGER,lb=0,ub=1)
+if verbose: print("creating z+ vars",flush=True)
+z_plus = mdl.addVars(nproc,nmach,name="z+",vtype=GRB.BINARY)
+for p in range(nproc):
+    for m in range(nmach):
+        z_minus[p,m].start=0
+        z_plus[p,m].start=0
 
+if verbose: print("creating y",flush=True)
+y = mdl.addVars(nproc,nmach,nmach,vtype=GRB.BINARY,name="y")
 
-print("creating y",flush=True)
-y = mdl.addVars(nproc,nmach,nmach,vtype=GRB.INTEGER,lb=0,ub=1,name="y")
+for p in range(nproc):
+    for i in range(nmach):
+        for j in range(nmach):
+            y[p,i,j].start=0
 
-print("creating t",flush=True)
+if verbose: print("creating t",flush=True)
 t = mdl.addVars(nmach,nmach,vtype=GRB.SEMIINT,lb=0,name="t")
 
-print("creating o",flush=True)
-o = mdl.addVars(nserv,len(L),vtype=GRB.BINARY,name="o")
-
-print("creating g",flush=True)
-g = mdl.addVars(nserv,vtype=GRB.BINARY,name="s")
-
-print("creating h",flush=True)
-h = mdl.addVars(nserv,len(N),vtype=GRB.BINARY,name="h")
-
-print("creating b",flush=True)
-b = mdl.addVars(nmach,nres,nres,vtype=GRB.SEMIINT,name="b")
-
-print("creating d",flush=True)
-d = mdl.addVars(nmach,nres,name="d",vtype=GRB.SEMIINT,lb=0)
-
-print("creating u",flush=True)
+if verbose: print("creating u",flush=True)
 u = mdl.addVars(nmach,nres,name="u",vtype=GRB.SEMIINT,lb=0,ub=C)
 
-print("creating a",flush=True)
+if verbose: print("creating a",flush=True)
 a = mdl.addVars(nmach,nres,name="a",vtype=GRB.SEMIINT,lb=0,ub=C)
 
+if verbose: print("creating d",flush=True)
+d = mdl.addVars(nmach,nres,name="d",vtype=GRB.SEMIINT,lb=0)
 
-print("creating obj vars",flush=True)
+if verbose: print("creating b",flush=True)
+b = mdl.addVars(nmach,nres,nres,vtype=GRB.SEMIINT,name="b")
+
+if verbose: print("creating o",flush=True)
+o = mdl.addVars(nserv,len(L),vtype=GRB.BINARY,name="o")
+
+if verbose: print("creating g",flush=True)
+g = mdl.addVars(nserv,vtype=GRB.BINARY,name="s")
+
+if verbose: print("creating h",flush=True)
+h = mdl.addVars(nserv,len(N),vtype=GRB.BINARY,name="h")
+
+for s in range(nserv):
+    for n in range(len(N)):
+        h[s,n].start=0
+
+if verbose: print("creating obj vars",flush=True)
 loadcost = mdl.addVars(nres,name="locadcost",vtype=GRB.SEMIINT,obj=Wlc)
 balancecost= mdl.addVars(nres,nres,name="balancecost",vtype=GRB.SEMIINT,obj=Wbal)
 pmc = mdl.addVar(name="pmc",vtype=GRB.SEMIINT,obj=WPMC)
 smc = mdl.addVar(name="smc",vtype=GRB.SEMIINT,obj=WSMC)
 mmc = mdl.addVar(name="mmc",vtype=GRB.SEMIINT,obj=WMMC)
 
-print("model update",flush=True)
+mdl_vars = time() - mdl_start
+
+if verbose: print("model update",flush=True)
 mdl.update()
 
-
-print("constr. all proc assigned")
+if verbose: print("constr. all proc assigned")
 mdl.addConstrs((x.sum(p,'*') == 1 for p in range(nproc)), name="process_assigned")
 
-print("constr. utilization")
+if verbose: print("constr. utilization")
 mdl.addConstrs((u[m,r] == quicksum(x[p,m]*R[p,r] for p in range(nproc)) for r in range(nres) for m in range(nmach)),name="utilization")
 
-print("constr. util < cap")
+if verbose: print("constr. util < cap")
 mdl.addConstrs((u[m,r] <= C[m,r] for r in range(nres) for m in range(nmach)),
                name="cap")
 
-print("constr. avail")
+if verbose: print("constr. avail")
 mdl.addConstrs((a[m,r] == C[m,r] - u[m,r] for r in range(nres) for m in range(nmach)),
                name="avail")
 
-print("constr. transient")
+if verbose: print("constr. transient")
 for r in range(nres):
     if T[r]:
         mdl.addConstrs((quicksum(x[p,m]*R[p,r] for p in range(nproc)) + quicksum(z_minus[p,m]*R[p,r] for p in range(nproc)) <= C[m,r] for m in range(nmach)),name="transient")
 
-print("constr. overload")
+if verbose: print("constr. overload")
 mdl.addConstrs((u[m,r] - d[m,r] <= C_bar[m,r] for r in range(nres) for m in range(nmach)),name="overload")
 
-print("constr. loadcost")
-mdl.addConstrs((loadcost[r] == quicksum(d[m,r] for m in range(nmach)) for r in range(nres)), name="loadcost")
 
-print("constr. z*[p,m]")    
+if verbose: print("constr. z*[p,m]")    
 
 mdl.addConstrs((z_plus[p,m] >= x[p,m] - x_bar[p,m] for  p in range(nproc) for m in range(nmach) ))
 mdl.addConstrs((z_minus[p,m] >= x_bar[p,m] - x[p,m] for  p in range(nproc) for m in range(nmach)) )
 
-print("constr. services")
-mdl.addConstrs((quicksum(x[p,m] for p in S[s])<=1 for s in range(len(S)) for m in range(nmach)), name="service")
+if verbose: print("constr. conflito")
+mdl.addConstrs((quicksum(x[p,m] for p in S[s])<=1 for s in range(len(S)) for m in range(nmach)), name="conflito")
 
-print("constr. y[p,i,j] >= z- + z+")
-mdl.addConstrs((y[p,i,j] >= z_minus[p,i] + z_plus[p,j] - 1 for p in range(nproc) for i in range(nmach) for j in range(nmach)), name="y")
-#print("constr. y[p,i,j] <= z-")
-#mdl.addConstrs((y[p,i,j] <= z_minus[p,i] for p in range(nproc) for i in range(nmach) for j in range(nmach)), name="y_z-")
-#print("constr. y[p,i,j] <= z+")
-#mdl.addConstrs((y[p,i,j] <= z_plus[p,j]  for p in range(nproc) for i in range(nmach) for j in range(nmach)), name="y_z+")
+if verbose: print("constr. 2*y[p,i,j] >= x_bar[pi] + x[pj]")
+mdl.addConstrs((2*y[p,i,j] >= z_minus[p,i] + z_plus[p,j] for p in range(nproc) for i in range(nmach) for j in range(nmach)), name="y")
 
-print("constr. t[i,j]=sum(y[p,i,j])")
+if verbose: print("constr. t[i,j]=sum(y[p,i,j])")
 mdl.addConstrs((t[i,j]==quicksum(y[p,i,j] for p in range(nproc)) for i in range(nmach) for j in range(nmach)),name="t")
 
-print("constr. o[s,l]")
+if verbose: print("constr. o[s,l]")
 for s in range(nserv):
     for l in range(len(L)):
         mdl.addConstrs((o[s,l] >= x[p,m] for p in S[s] for m in L[l]),name=("o[%d,%d]"%(s,l)))
+        mdl.addConstr((o[s,l]<=quicksum((x[p,m] for p in S[s] for m in L[l]))), name="o_ub")
+        
+mdl.addConstrs((quicksum(o[s,l] for l in range(len(L))) >= delta[s] for s in range(nserv)),name="serv_spread")
 
-mdl.addConstrs((quicksum(o[s,l] for l in range(len(L))) >= spread[s]) for s in range(nserv))
-
-#print("constr. k[p,n]")
-#mdl.addConstrs((k[p,n] == quicksum(x[p,m] for m in N[n]) for p in range(nproc) for n in range(len(N))),name="k")
-
-print("constr. g[s]")
+if verbose: print("constr. g[s]")
 mdl.addConstrs((g[s] == quicksum(z_plus[p,m] for m in range(nmach) for p in S[s]) for s in range(nserv)), name="g")
 
-print("constr. h[s,n]")
+if verbose: print("constr. h[s,n]")
 for s in range(nserv):
     for n in range(len(N)):
         mdl.addConstrs((h[s,n] >= x[p,m] for p in S[s] for m in N[n]),name=("h[%d,%d]"%(s,l)))
+        mdl.addConstr((h[s,n] <= quicksum((x[p,m] for p in S[s] for m in N[n]))),name="h_upperbound")
 
 for s in range(nserv):
     if s in sdep:
-        print((s,sdep[s]))
+        if verbose: print((s,sdep[s]))
         for _s in sdep[s]:
             mdl.addConstrs((h[s,n] <= h[_s,n] for n in range(len(N))),name=("dep[%d,%d]"%(s,_s)))
 
-print("constr. b[m,r1,r2]")
+if verbose: print("constr. b[m,r1,r2]")
 mdl.addConstrs((b[m,r1,r2] >= bT[r1,r2]*a[m,r1] - a[m,r2] for m in range(nmach) for r1 in range(nres) for r2 in range(nres)), name="b")    
+
+
+if verbose: print("constr. loadcost")
+mdl.addConstrs((loadcost[r] == quicksum(d[m,r] for m in range(nmach)) for r in range(nres)), name="loadcost")
 
 mdl.addConstrs((balancecost[r1,r2] == quicksum(b[m,r1,r2] for m in range(nmach)) for r1 in range(nres) for r2 in range(nres)))
 
-print("constr. PMC")
-mdl.addConstr(pmc == quicksum(PMC[p]*z_plus[p,m] for m in range(nmach) for p in range(nproc)))
+if verbose: print("constr. PMC")
+mdl.addConstr(pmc == quicksum(RHO[p]*z_plus[p,m] for m in range(nmach) for p in range(nproc)))
 
-print("constr. SMC")
+if verbose: print("constr. SMC")
 mdl.addConstrs(smc >= g[s] for s in range(nserv))
 
-print("constr. MMC")
-mdl.addConstr(mmc == quicksum(MMC[i,j]*t[i,j] for i in range(nmach) for j in range(nmach)))
+if verbose: print("constr. MMC")
+mdl.addConstr(mmc == quicksum(MU[i,j]*t[i,j] for i in range(nmach) for j in range(nmach)))
 
+mdl_constrs = time() - mdl_start - mdl_vars
 
-#mdl.Params.OutputFlag=0
-#mdl.Params.PoolSolutions = 1000
-#mdl.Params.Threads = 1
-#mdl.Params.MIPFocus = 3
-#mdl.Params.Heuristics = 1
-
-print("save model")
-mdl.write(name + ".mps")
-mdl.write(name + ".lp")
-mdl._lastiter = -GRB.INFINITY
-mdl._lastnode = -GRB.INFINITY
+if savemodel:
+    if verbose: print("save model")
+    mdl.write(name + ".mps")
+    mdl.write(name + ".lp")
 
 
 def cb(model,where):
+    """ Callback
+
+    O modelo terminará antes, aceitando um gap de 5%, pois queremos medir a 
+    qualidade do modelo, em relação a fornecer soluções viáveis.  
+    Posteriormente será removido este callback.
+    """
     if where == GRB.Callback.MIP:
         # General MIP callback
-        nodecnt = model.cbGet(GRB.Callback.MIP_NODCNT)
         objbst = model.cbGet(GRB.Callback.MIP_OBJBST)
         objbnd = model.cbGet(GRB.Callback.MIP_OBJBND)
-        solcnt = model.cbGet(GRB.Callback.MIP_SOLCNT)
         if abs(objbst - objbnd) < 0.05 * (1.0 + abs(objbst)):
-            print('>>>> Stop early - 5% gap achieved')
+            if verbose: print('>>>> Stop early - 5% gap achieved')
             model.terminate()
 
 mdl._x=x
-print("optimize")
+if verbose: print("optimize")
 _start = time()
 mdl.optimize(cb)
 _elapsed = time() - _start
+mdl_opt=_elapsed
 
-print('Optimization was stopped with status ' + str(mdl.Status),flush=True)
+if verbose: print('Optimization was stopped with status ' + str(mdl.Status),flush=True)
 
-print(">>> optimized in %0.2f" % _elapsed ,flush=True)
+if verbose: print(">>> optimized in %0.2f" % _elapsed ,flush=True)
+
+if verbose: print(">>> Optimal value: %d" % mdl.objVal)
 
 for r in range(nres):
-    print("resource obj %d: %d" % (r, loadcost[r].X))
+    if verbose: print("resource obj %d: %d" % (r, loadcost[r].X))
 for r1 in range(nres):
     for r2 in range(nres):
-        print("balance (%d,%d): %d"%(r1,r2,balancecost[r1,r2].X))
-print("PMC            : %d" %pmc.X)
-print("SMC            : %d" %smc.X)
-print("MMC            : %d" %mmc.X)
+        if verbose: print("balance (%d,%d): %d"%(r1,r2,balancecost[r1,r2].X))
+if verbose: print("PMC            : %d" %pmc.X)
+if verbose: print("SMC            : %d" %smc.X)
+if verbose: print("MMC            : %d" %mmc.X)
 
 solution=[]
 for p in range(nproc):
@@ -343,12 +368,20 @@ for p in range(nproc):
         if x[p,m].X >0.5 :
             solution.append(m)
 
-print(assign)
-print(solution)
+if verbose: print(assign)
+if verbose: print(solution)
 
-print([ "proc %d: %d -> %d" %(n,k[0],k[1])  for n,k in enumerate(zip(assign,solution)) if k[0]!=k[1]], sep="\n")
+if verbose: print([ "proc %d: %d -> %d" %(n,k[0],k[1])  for n,k in enumerate(zip(assign,solution)) if k[0]!=k[1]], sep="\n")
 
 if outputfile:
-    print(">>> salvando resposta em %s" % outputfile)
+    if verbose: print(">>> salvando resposta em %s" % outputfile)
     f = open(outputfile, "w")
     f.write(' '.join(str(i) for i in solution))
+    f.close()
+
+all_time = time() - all_begin
+if tex:
+    import subprocess
+    import resource
+    output = subprocess.check_output(["./checker", modelfile,assignfile,outputfile])
+    print("\\verb|%s| & %g & %g & %g & %g & %g & %d & %s \\\\" % (name, all_time, load_time, mdl_vars, mdl_constrs, mdl_opt,resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024 +1 , output.decode("utf-8").strip('\n')))
