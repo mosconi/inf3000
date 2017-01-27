394b511506bdfad86d5e8f047ed7e8deede79769
Rodrigo Mosconi <mosconi.rmg@gmail.com>
2017-01-27 04:20:24 -0200

    primeira versão do modelo incremental

--- src/milp_steped.py
+++ src/milp_steped.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 
 import os, sys, getopt
@@ -15,7 +16,7 @@ name="roadef"
 outputfile=None
 savemodel=False
 verbose=True
-tex=True
+tex=False
 
 try:
     opts, args = getopt.getopt(sys.argv[1:],"tqsn:m:a:o:h",["tex","quiet","save","name=","model=","assign=","output=","help"])
@@ -30,6 +31,7 @@ for o,a in opts:
         sys.exit()
     elif o in ( "-a", "--assign"):
         assignfile = a
+
     elif o in ( "-m", "--model"):
         modelfile = a
     elif o in ( "-n", "--name"):
@@ -165,14 +167,36 @@ if verbose: print(">>> balances ",nbal)
 if verbose: print(">>> neighborhood ",len(N))
 if verbose: print(">>> locations ",len(L))
 
-mdl_start = time()
-load_time = mdl_start - all_begin
+load_time = time() - all_begin
 
 mdl=Model(name)
 if not verbose: mdl.Params.OutputFlag=0
 
 mdl.ModelSense = GRB.MINIMIZE
+mdl._objVal = {}
+mdl._objbnd = {}
+def cb(model,where):
+    """ Callback
 
+    O modelo terminará antes, aceitando um gap de 5%, pois queremos medir a 
+    qualidade do modelo, em relação a fornecer soluções viáveis.  
+    Posteriormente será removido este callback.
+    """
+    return
+    if where == GRB.Callback.MIPNODE:
+        if model._step not in model._objbnd: model._objbnd[model._step] = model.cbGet(GRB.Callback.MIPNODE_OBJBND)
+    elif where == GRB.Callback.MIP:
+        if model._step not in model._objbnd: model._objbnd[model._step] = model.cbGet(GRB.Callback.MIPNODE_OBJBND)
+        # General MIP callback
+        objbst = model.cbGet(GRB.Callback.MIP_OBJBST)
+        _objbnd = model._objbnd[model._step]
+        # objbnd = model.cbGet(GRB.Callback.MIP_OBJBND)
+        if (_objbnd - objbst) < (0.05 * _objbnd):
+            if verbose: print('>>>> Stop early - %d %% gap achieved' % (5))
+            model.terminate()
+
+mdl_start = time()
+_start = time()
 if verbose: print("creating x vars",flush=True)
 x = mdl.addVars(nproc,nmach,vtype=GRB.BINARY,lb=0,ub=1,name="x")
 
@@ -181,16 +205,14 @@ for p in range(nproc):
     for m in range(nmach):
         x[p,m].start = x_bar[p,m]
 
-if verbose: print("creating z- vars",flush=True)
-z_minus = mdl.addVars(nproc,nmach,name="z-",vtype=GRB.BINARY)
+if verbose: print("creating z vars",flush=True)
+z = mdl.addVars(nproc,nmach,name="z",vtype=GRB.BINARY)
 
 if verbose: print("creating u",flush=True)
-u = mdl.addVars(nmach,nres,name="u",vtype=GRB.SEMIINT,lb=0,ub=C)
-
+u = mdl.addVars(nmach,nres,name="u",vtype=GRB.INTEGER,lb=0,ub=C)
 
 if verbose: print("creating d",flush=True)
-d = mdl.addVars(nmach,nres,name="d",vtype=GRB.SEMIINT,lb=0)
-
+d = mdl.addVars(nmach,nres,name="d",vtype=GRB.INTEGER,lb=0)
 
 if verbose: print("creating o",flush=True)
 o = mdl.addVars(nserv,len(L),vtype=GRB.BINARY,name="o") 
@@ -203,10 +225,11 @@ for s in range(nserv):
         h[s,n].start=0
 
 if verbose: print("creating obj vars",flush=True)
-loadcost = mdl.addVars(nres,name="locadcost",vtype=GRB.SEMIINT,obj=Wlc)
+loadcost = mdl.addVars(nres,name="locadcost",vtype=GRB.INTEGER,obj=Wlc)
 
-mdl_vars = time() - mdl_start
+mdl_vars1 = time() - _start
 
+_start = time()
 if verbose: print("model update",flush=True)
 mdl.update()
 
@@ -223,7 +246,7 @@ mdl.addConstrs((u[m,r] <= C[m,r] for r in range(nres) for m in range(nmach)),
 if verbose: print("constr. transient (hard)")
 for r in range(nres):
     if T[r]:
-        mdl.addConstrs((quicksum(x[p,m]*R[p,r] for p in range(nproc)) + quicksum(z_minus[p,m]*R[p,r] for p in range(nproc)) <= C[m,r] for m in range(nmach)),name="transient")
+        mdl.addConstrs((quicksum(x[p,m]*R[p,r] for p in range(nproc)) + quicksum(z[p,m]*R[p,r] for p in range(nproc)) <= C[m,r] for m in range(nmach)),name="transient")
 
 if verbose: print("constr. overload (obj)")
 mdl.addConstrs((u[m,r] - d[m,r] <= C_bar[m,r] for r in range(nres) for m in range(nmach)),name="overload")
@@ -256,14 +279,7 @@ for s in range(nserv):
 if verbose: print("constr. loadcost")
 mdl.addConstrs((loadcost[r] == quicksum(d[m,r] for m in range(nmach)) for r in range(nres)), name="loadcost")
 
-
-mdl_constrs = time() - mdl_start - mdl_vars
-
-if savemodel:
-    if verbose: print("save model")
-    mdl.write(name + ".mps")
-    mdl.write(name + ".lp")
-
+mdl_constrs1 = time() - _start 
 
 def cb(model,where):
     """ Callback
@@ -272,111 +288,133 @@ def cb(model,where):
     qualidade do modelo, em relação a fornecer soluções viáveis.  
     Posteriormente será removido este callback.
     """
-    if where == GRB.Callback.MIP:
-        # General MIP callback
-        objbst = model.cbGet(GRB.Callback.MIP_OBJBST)
-        objbnd = model.cbGet(GRB.Callback.MIP_OBJBND)
-        if abs(objbst - objbnd) < 0.05 * (1.0 + abs(objbst)):
-            if verbose: print('>>>> Stop early - 5% gap achieved')
-            model.terminate()
+    if where == GRB.Callback.MIPNODE:
+        if model._step not in model._objbnd: model._objbnd[model._step] = model.cbGet(GRB.Callback.MIPNODE_OBJBND)
+    return
+    #elif where == GRB.Callback.MIP:
+    #    if model._step not in model._objbnd: model._objbnd[model._step] = model.cbGet(GRB.Callback.MIPNODE_OBJBND)
+    #    # General MIP callback
+    #    objbst = model.cbGet(GRB.Callback.MIP_OBJBST)
+    #    _objbnd = model._objbnd[model._step]
+    #    # objbnd = model.cbGet(GRB.Callback.MIP_OBJBND)
+    #    if (_objbnd - objbst) < (0.01 * (6 - model._step) * _objbnd):
+    #        if verbose: print('>>>> Stop early - %d  gap achieved' % (6- model._step))
+    #        model.terminate()
 
 mdl._x=x
+mdl._step=1
 if verbose: print("optimize step 1")
 _start = time()
 mdl.optimize(cb)
-_elapsed = time() - _start
-mdl_opt=_elapsed
+mdl_opt1= time() - _start
 
-if nbal>0:
-    if verbose: print("creating a",flush=True)
-    a = mdl.addVars(nmach,nres,name="a",vtype=GRB.SEMIINT,lb=0,ub=C)
+mdl._objVal[1] = mdl.objVal
+all_time1 = time() - mdl_start
 
-    if verbose: print("creating b",flush=True)
-    b = mdl.addVars(nmach,nres,nres,vtype=GRB.SEMIINT,name="b")
+mdl_start = time()
+_start = time()
+if verbose: print("creating a",flush=True)
+a = mdl.addVars(nmach,nres,name="a",vtype=GRB.INTEGER,lb=0,ub=C)
 
-    balancecost= mdl.addVars(nres,nres,name="balancecost",vtype=GRB.SEMIINT,obj=Wbal)
+if verbose: print("creating b",flush=True)
+b = mdl.addVars(nmach,nres,nres,vtype=GRB.INTEGER,name="b")
 
-    mdl.update()
-
-    if verbose: print("constr. avail")
-    mdl.addConstrs((a[m,r] == C[m,r] - u[m,r] for r in range(nres) for m in range(nmach)),name="avail")
-
-    if verbose: print("constr. b[m,r1,r2] (obj)")
-    mdl.addConstrs((b[m,r1,r2] >= bT[r1,r2]*a[m,r1] - a[m,r2] for m in range(nmach) for r1 in range(nres) for r2 in range(nres)), name="b")
-
-    mdl.addConstrs((balancecost[r1,r2] == quicksum(b[m,r1,r2] for m in range(nmach)) for r1 in range(nres) for r2 in range(nres)),name="balancecost")
-
-    if verbose: print("optimize step2")
-    mdl.optimize(cb)
-
-
-
-
-if verbose: print("creating z+ vars",flush=True)
-z_plus = mdl.addVars(nproc,nmach,name="z+",vtype=GRB.BINARY)
-for p in range(nproc):
-    for m in range(nmach):
-        z_minus[p,m].start=0
-        z_plus[p,m].start=0
-
-pmc = mdl.addVar(name="pmc",vtype=GRB.SEMIINT,obj=WPMC)
-
+balancecost= mdl.addVars(nres,nres,name="balancecost",vtype=GRB.INTEGER,obj=Wbal)
+mdl_vars2 = time() - _start
+_start = time()
 mdl.update()
 
-if verbose: print("constr. z*[p,m]")    
+if verbose: print("constr. avail")
+mdl.addConstrs((a[m,r] == C[m,r] - u[m,r] for r in range(nres) for m in range(nmach)),name="avail")
 
-mdl.addConstrs((z_plus[p,m] >= x[p,m] - x_bar[p,m] for  p in range(nproc) for m in range(nmach) ))
-mdl.addConstrs((z_minus[p,m] >= x_bar[p,m] - x[p,m] for  p in range(nproc) for m in range(nmach)) )
+if verbose: print("constr. b[m,r1,r2] (obj)")
+mdl.addConstrs((b[m,r1,r2] >= bT[r1,r2]*a[m,r1] - a[m,r2] for m in range(nmach) for r1 in range(nres) for r2 in range(nres)), name="b")
+
+mdl.addConstrs((balancecost[r1,r2] == quicksum(b[m,r1,r2] for m in range(nmach)) for r1 in range(nres) for r2 in range(nres)),name="balancecost")
+
+mdl_constrs2 = time() - _start
+mdl._step=2
+if verbose: print("optimize step2")
+_start = time()
+mdl.optimize(cb)
+_elapsed = time() - _start
+mdl_opt2=_elapsed
+
+mdl._objVal[2] = mdl.objVal
+all_time2 = time() - mdl_start
+    
+mdl_start = time()
+
+_start = time()
+pmc = mdl.addVar(name="pmc",vtype=GRB.INTEGER,obj=WPMC)
+mdl_vars3 = time() - _start
+
+_start = time()
+mdl.update()
 
 if verbose: print("constr. PMC")
-mdl.addConstr(pmc == quicksum(RHO[p]*z_plus[p,m] for m in range(nmach) for p in range(nproc)))
-
+mdl.addConstr(pmc == quicksum(RHO[p]*z[p,m] for m in range(nmach) for p in range(nproc)))
+mdl_constrs3 = time() - _start
+mdl._step=3
 if verbose: print("optimize step3")
 mdl.optimize(cb)
-        
+mdl_opt3 = time() - _start
+mdl._objVal[3] = mdl.objVal
 
+all_time3 = time() - mdl_start
 
+mdl_start = time()
+
+_start = time()
 if verbose: print("creating g",flush=True)
 g = mdl.addVars(nserv,vtype=GRB.BINARY,name="g")
        
-smc = mdl.addVar(name="smc",vtype=GRB.SEMIINT,obj=WSMC)
+smc = mdl.addVar(name="smc",vtype=GRB.INTEGER,obj=WSMC)
+mdl_vars4 = time() - _start
 
+_start = time() 
 mdl.update()
 if verbose: print("constr. g[s]")
-mdl.addConstrs((g[s] == quicksum(z_plus[p,m] for m in range(nmach) for p in S[s]) for s in range(nserv)), name="g")
+mdl.addConstrs((g[s] == quicksum(z[p,m] for m in range(nmach) for p in S[s]) for s in range(nserv)), name="g")
 
 if verbose: print("constr. SMC")
 mdl.addConstrs(smc >= g[s] for s in range(nserv))
+mdl_constrs4 = time()  - _start
 
+mdl._step=4
 if verbose: print("optimize step4")
+_start = time()
 mdl.optimize(cb)
+mdl_opt4 = time() - _start
 
-if verbose: print("creating y",flush=True)
-y = mdl.addVars(nproc,nmach,nmach,vtype=GRB.BINARY,name="y")
+mdl._objVal[4] = mdl.objVal
+all_time4 = time() - mdl_start
 
+mdl_start = time()
 if verbose: print("creating t",flush=True)
-t = mdl.addVars(nmach,nmach,vtype=GRB.SEMIINT,lb=0,name="t")
+_start = time()
+t = mdl.addVars(nmach,nmach,vtype=GRB.INTEGER,lb=0,name="t")
 
-for p in range(nproc):
-    for i in range(nmach):
-        for j in range(nmach):
-            y[p,i,j].start=0
+mmc = mdl.addVar(name="mmc",vtype=GRB.INTEGER,lb=0,obj=WMMC)
+mdl_vars5 = time() - _start
 
-mmc = mdl.addVar(name="mmc",vtype=GRB.SEMIINT,obj=WMMC)
-
+_start = time()
 mdl.update()
 
-if verbose: print("constr. 2*y[p,i,j] >= x_bar[pi] + x[pj]")
-mdl.addConstrs((2*y[p,i,j] >= z_minus[p,i] + z_plus[p,j] for p in range(nproc) for i in range(nmach) for j in range(nmach)), name="y")
-
-if verbose: print("constr. t[i,j]=sum(y[p,i,j])")
-mdl.addConstrs((t[i,j]==quicksum(y[p,i,j] for p in range(nproc)) for i in range(nmach) for j in range(nmach)),name="t")
+if verbose: print("constr. t[i,j]=sum(x_bar[p,i]*x[p,j])")
+mdl.addConstrs((t[i,j]==quicksum(x_bar[p,i]*x[p,j] for p in range(nproc)) for i in range(nmach) for j in range(nmach)),name="t")
 
 if verbose: print("constr. MMC")
 mdl.addConstr(mmc == quicksum(MU[i,j]*t[i,j] for i in range(nmach) for j in range(nmach)))
 
+mdl_constrs5 = time() - _start
+mdl._step=5
 if verbose: print("optimize step5")
+_start = time()
 mdl.optimize(cb)
+mdl_opt5 = time() - _start
+mdl._objVal[5] = mdl.objVal
+all_time5 = time() - mdl_start
 
 if verbose: print('Optimization was stopped with status ' + str(mdl.Status),flush=True)
 
@@ -415,7 +453,31 @@ if tex:
     import subprocess
     import resource
     output = subprocess.check_output(["./checker", modelfile,assignfile,outputfile])
-    print("\\verb|%s| & %g & %g & %g & %g & %g & %d & %s \\\\" % (name, all_time, load_time, mdl_vars, mdl_constrs, mdl_opt,resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024 +1 , output.decode("utf-8").strip('\n')))
-
-
+    print("\\verb|%s| & %0.3f & %0.3f & "
+          " %0.3f & %0.3f & %0.3f & %0.3f &"
+          " %0.3f & %0.3f & %0.3f & %0.3f &"
+          " %0.3f & %0.3f & %0.3f & %0.3f &"
+          " %0.3f & %0.3f & %0.3f & %0.3f &"
+          " %0.3f & %0.3f & %0.3f & %0.3f &"
+          " %d &"
+          " %0.3f & %0.3f & %0.3f & "
+          " %0.3f & %0.3f & %0.3f & "
+          " %0.3f & %0.3f & %0.3f & "
+          " %0.3f & %0.3f & %0.3f & "
+          " %0.3f & %0.3f & %0.3f & "
+          " %s" %
+          (name, all_time, load_time,
+           all_time1, mdl_vars1, mdl_constrs1, mdl_opt1,
+           all_time2, mdl_vars2, mdl_constrs2, mdl_opt2,
+           all_time3, mdl_vars3, mdl_constrs3, mdl_opt3,
+           all_time4, mdl_vars4, mdl_constrs4, mdl_opt4,
+           all_time5, mdl_vars5, mdl_constrs5, mdl_opt5,
+           resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024 +1 , 
+           mdl._objbnd[1], mdl._objVal[1],  (abs(mdl._objbnd[1]-mdl._objVal[1])/abs(mdl._objbnd[1]))*100 ,
+           mdl._objbnd[2], mdl._objVal[2],  (abs(mdl._objbnd[2]-mdl._objVal[2])/abs(mdl._objbnd[2]))*100 ,
+           mdl._objbnd[3], mdl._objVal[3],  (abs(mdl._objbnd[3]-mdl._objVal[3])/abs(mdl._objbnd[3]))*100 ,
+           mdl._objbnd[4], mdl._objVal[4],  (abs(mdl._objbnd[4]-mdl._objVal[4])/abs(mdl._objbnd[4]))*100 ,
+           mdl._objbnd[4], mdl._objVal[4],  (abs(mdl._objbnd[4]-mdl._objVal[4])/abs(mdl._objbnd[4]))*100 ,
+           output
+          ))
 
