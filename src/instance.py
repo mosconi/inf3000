d9acc1f11d42934e29468f7edade6a81d669842d
Rodrigo Mosconi <mosconi.rmg@gmail.com>
2017-04-12 19:01:06 -0300

    ok

--- src/instance.py
+++ src/instance.py
@@ -8,19 +8,26 @@ class Instance:
         return super(Instance,cls).__new__(cls)
     
     def __init__ (self,model=None,assign=None):
-        modelfile=model
-        assignfile=assign
 
-        import numpy as np
 
+        self._loadmodel(model)
+        self._loadassign(assign)
+
+    def _loadmodel(self,_modelfile):
 
         self.M=[]
 
-        f = open(modelfile, "r")
+        f = open(_modelfile, "r")
 
         lines = [list(map(int,l.rstrip('\n').split())) for l in f]
 
-        self.nres = lines.pop(0)[0]
+        f.close()
+
+        import numpy as np
+
+        nres = lines.pop(0)[0]
+
+        self.nres = nres
 
         self.T=np.zeros(self.nres,dtype=np.bool)
         self.Wlc=np.zeros(self.nres,dtype=np.int32)
@@ -57,7 +64,8 @@ class Instance:
         self.N = [n for n in N if n]
 
     
-        self.nserv = lines.pop(0)[0]
+        nserv = lines.pop(0)[0]
+        self.nserv=nserv
         delta=[0 for s in range(nserv)]
         sdep={}
 
@@ -67,7 +75,9 @@ class Instance:
             if l[1]>0:
                 sdep[s]=l[2:]
 
-        self.nproc = lines.pop(0)[0]
+        nproc = lines.pop(0)[0]
+
+        self.nproc = nproc
 
         self.S=[]
         self.R=np.zeros((self.nproc,self.nres),dtype=np.int32)
@@ -98,8 +108,51 @@ class Instance:
         self.WSMC=lines[0][1]
         self.WMMC=lines[0][2]
     
-        self.R = np.array(R,dtype=np.int32)
         self.S = [s for s in S if s]    
 
-        self.RHO=np.array(RHO,dtype=np.int32)
+
+    def _loadassign(self, _assignfile):
+        f = open(_assignfile, "r")
+        line = [list(map(int,l.rstrip('\n').split())) for l in f][0]
+        assign=line[:]
+
+
+    def validate(self,solution=None):
+
+        if not solution:
+            raise("")
+
+ 
+        import numpy as np
+        moved_proc = [n for n,k in enumerate(zip(self.assign,solution)) if k[0]!=k[1]]
+
+        print(moved_proc)
+
+        q=[[] for m in range(self.nmach)]
+
+        q[m].append(np.array([solution[p]==m for p in range(self.nproc)],dtype=np.int32))
+        _util = (R*q[m][0].reshape(self.nproc,1)).sum(axis=0) 
+#        _util_tr =  
+        _obj1= _util - C_bar[m]
+        _obj1[_obj1<0]=0
+        
+        _avail = C[m] - _util
+        _obj2=np.array([[bT[r1,r2]*_avail[r1] - _avail[r2] for r2 in range(nres)] for r1 in range(self.nres)],dtype=np.int32)
+        _obj2[_obj2<0] = 0
+    
+        _obj3 = WPMC*RHO*q
+        _obj3[[p for p in range(self.nproc) if p in mach_assign[m]]]=0
+
+        _obj5 = MU[solution,m]*q
+
+        _obj = (Wlc*_obj1).sum() + (Wbal*_obj2).sum() + (_obj3).sum() + (WMMC*_obj5).sum()
+
+    def objective(self,assign=None):
+        
+        if not assign:
+            raise("")
+
+        moved_proc = [n for n,k in enumerate(zip(assign,solution)) if k[0]!=k[1]]
+
+        
 
