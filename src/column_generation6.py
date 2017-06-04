8f7562d8ad8a2686219da33864bc031f03165ecf
Rodrigo Mosconi <mosconi.rmg@gmail.com>
2017-06-03 23:08:40 -0300

    col generation take 6

--- src/column_generation6.py
+++ src/column_generation6.py
@@ -71,8 +71,64 @@ if not os.path.exists(assignfile):
 
 inst = Instance(model=modelfile, assign=assignfile)
 
-
 cg = CG5(inst, epslon=0.5)
 
-last_omega =  -float('Inf')
 
+best_omega =  -float('Inf')
+
+continue_cond = True
+
+best_pi = np.zeros(inst.nproc)
+best_alpha = np.zeros(inst.nmach)
+best_omega = - np.inf
+
+beta = .5
+
+cg.build_model()
+
+k=0
+
+while continue_cond:
+    (z_rm, pi_rm, alpha_rm) = cg.solve_relax()
+
+    pi = beta * best_pi + ( 1 - beta ) * pi_rm
+    alpha = beta * best_alpha  + ( 1 - beta ) * alpha_rm
+
+    omega = 0
+    print(" iter %05d  obj: %+20.3f" %(k, z_rm) )
+    
+    for m in range(inst.nmach):
+        print(" iter %05d  mach %05d => " % (k,m),end='')
+        (roadef, w , q)= cg.compute_column(m, pi, alpha[m])
+#        if w > - epslon:
+#            print("for break")
+#            break
+
+        print("%20.3f (roadef: %20.3f rat: %20.10f)" % (w, roadef,abs(w/(roadef+1))))
+        omega += w
+        if len(q) < 100:
+            print("add col", q)
+        cg.mip_add_col(obj=roadef, col =q, machine=m)
+
+        #cols = cg.generate_companion_columns(machine=m,col=q,obj=roadef
+        #for (_m,_obj,_col) in cols 
+
+    print("omega: %20.3f  (best:  %20.3f delta: %20.3f rat(obj/best): %20.3f)" % (omega, best_omega, z_rm - best_omega, z_rm/best_omega))
+
+    if omega > best_omega:
+        print("best omega improvement %20.3f -> %20.3f" % (best_omega, omega))
+        best_omega = omega
+        best_pi = pi_rm
+        best_alpha = alpha_rm
+        input("Press Enter to continue...")
+
+    if abs(z_rm - best_omega) < epslon:
+        contine_cond = False
+        break
+        
+    k+=1
+
+
+(obj, X, alloc) = cg.solve_mip()
+
+print(X)
