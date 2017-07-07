9600be1e5441c0d2d086221c2549c545cdf0e443
Rodrigo Mosconi <mosconi.rmg@gmail.com>
2017-07-07 12:36:46 -0300

    script para rodar tudo

--- run_mip.sh
+++ run_mip.sh
@@ -7,9 +7,8 @@ timelimit=21600  # 6h
 models="mip1 lp1 mip2 lp2 mip3 lp3"
 
 for instance in ${instances}; do
-    echo ${instance}
     for model in ${models}; do
-        python src/${model} -t ${timelimit} -p data/model_${instance}.txt -i data/assignment_${instance}.txt -o output/out_${model}_${instance}.txt
+        bin/${model} -t ${timelimit} -p data/model_${instance}.txt -i data/assignment_${instance}.txt -o output/out_${model}_${instance}.txt
     done
 done
          
