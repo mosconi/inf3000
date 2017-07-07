dfd8fc086f2ad5b483b2ae446ca7bfac0af4e285
Rodrigo Mosconi <mosconi.rmg@gmail.com>
2017-07-07 12:34:43 -0300

    esqueleto de CG

---
+++ rmg/columngeneration/cg.py
@@ -0,0 +1,23 @@
+
+class CG(object):
+    _instance = None
+    _args = None
+    _mip = None
+    _lp = None
+    _env = None
+    
+    def __init__(self,instance,args):
+        self._args = args
+        self._args = instance
+
+    def build_model(self):
+        pass
+
+    def build_column_model(self,machine):
+        pass
+
+    def solve(self):
+        pass
+
+    def solve_mip(self):
+        pass
