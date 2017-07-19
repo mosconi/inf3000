584779a4ba597cb01b8c5d0f70d486221f259fd6
Rodrigo Mosconi <mosconi.rmg@gmail.com>
2017-07-18 23:33:10 -0300

    arrumando grupos de argumentos

--- rmg/columngeneration/parser.py
+++ rmg/columngeneration/parser.py
@@ -8,12 +8,8 @@ solver.add_argument("--epslon",type=float,default=1e-1,
                     help="Error tolerance (column generation)")
 solver.add_argument("--tol",type=float,default=1e-6,
                     help="Error tolerance (calculations)")
-solver.add_argument("--runname",dest="run_name",default="columngeneration",
-                    help="Name for the model")
-solver.add_argument("--novalidate",dest="validate",action="store_false",default=True,
+solver.add_argument("--validate",dest="validate",action="store_true",default=False,
                     help="Validate solver results.")
-solver.add_argument("--nodump",dest="dump",action="store_false",default=True,
-                    help="Dump models.")
 
 log_group = parser.add_argument_group("log","For solver logging")
 log_group.add_argument("--log",dest="log",default=False,action="store_true",
@@ -26,6 +22,10 @@ log_group.add_argument("--logfile",dest="logfile",action="store",default="column
 model = parser.add_argument_group("model","Options to change model behaviour")
 model.add_argument("--pregenerate",dest="generate",default=False,action="store_true",
                        help="Pre-generate some columns")
+model.add_argument("--dump",dest="dump",action="store_true",default=False,
+                    help="Dump models.")
+model.add_argument("--run_name",dest="run_name",default="columngeneration",
+                    help="Name for the model")
 
 
 stab = parser.add_argument_group("stabilization","Options to change dual stabilization")
